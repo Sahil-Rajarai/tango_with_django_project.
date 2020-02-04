d51fbb134fe1f01e7253e566591e732bfc60441a
Sahil Rajarai <sahil-007@live.com>
2020-02-04 02:55:27 +0000

    retrieve categories from db and display in index file

--- rango/views.py
+++ rango/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request) :
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    # Place the list in our context_dict dictionary (with our boldmessage!)
+    # that will be passed to the template engine.
+    # '-' in likes denotes descending order
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
     return render(request, 'rango/index.html', context=context_dict)
 
+# def show_category(request, category_name_slug) :
+
+    
 def about(request) :
     return render(request, 'rango/about.html')
 
